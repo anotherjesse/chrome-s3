f83dc01db1b3330bf87cc3a0d7568047bd50f390
Jesse Andrews <anotherjesse@gmail.com>
2010-01-22 21:16:39 -0800

    chrome extension based on s3://

---
+++ account.js
@@ -0,0 +1,136 @@
+// Copyright Jesse Andrews, 2005-2010
+// http://overstimulate.com
+//
+// This file may be used under the terms of of the
+// GNU General Public License Version 2 or later (the "GPL"),
+// http://www.gnu.org/licenses/gpl.html
+//
+// Software distributed under the License is distributed on an "AS IS" basis,
+// WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+// for the specific language governing rights and limitations under the
+// License.
+
+
+function setkeys() {
+  $('#account').show();
+}
+
+function s3Control() {
+  $('#updateCredentials').click(function() {
+    $.blockUI($('#credentials'));
+
+    return false;
+  });
+
+  $('#createBucket').click(function() {
+                             var params = {};
+                             window.openDialog("createBucket.xul",
+                                               "createS3Bucket",
+                                               "centerscreen,chrome,modal",
+                                               params);
+                             if (params.success) {
+                               list();
+                             }
+                             return false;
+                           });
+
+  function addBucket( bucket ) {
+    var tr=document.createElement('tr');
+    tr.setAttribute('id', bucket);
+
+    var td=document.createElement('td');
+    var a=document.createElement('a');
+    a.appendChild( document.createTextNode(bucket) );
+    a.setAttribute("href", "browse.html?" + encodeURIComponent(bucket));
+    td.appendChild( a );
+    tr.appendChild( td );
+
+    // var td=document.createElement('td');
+    // var a=document.createElement('a');
+    // a.appendChild( document.createTextNode('delete') );
+    // a.setAttribute('class', 'delete');
+    // a.setAttribute('style', 'display: none');
+    // a.setAttribute('href', '#');
+    // a.setAttribute("bucket", bucket);
+    // td.appendChild( a );
+    tr.appendChild( td );
+
+    document.getElementById('buckets').appendChild(tr);
+
+    $(a).click(function() {
+      S3Ajax.deleteBucket(this.getAttribute('bucket'), list, function(req) {
+        humanMsg.displayMsg('<strong>' + bucket + '</strong>: ' +
+	  req.responseXML.getElementsByTagName('Message')[0].childNodes[0].textContent);
+      });
+      return false;
+    });
+  }
+
+  function list() {
+    $('#active').show();
+    $('#active').addClass('busy');
+    $('#buckets').hide().empty();
+
+    S3Ajax.listBuckets(
+      function(xml, objs) {
+        foo = xml;
+        bar = objs;
+        var buckets = xml.responseXML.getElementsByTagName('Bucket');
+        for (var i=0; i<buckets.length; i++) {
+          addBucket(buckets[i].getElementsByTagName('Name')[0].textContent);
+        }
+        $('#buckets').show();
+        // $('#createBucket').animate({opacity: 'show'});
+        $('#active').removeClass('busy');
+      },
+      function(req) {
+        humanMsg.displayMsg(req.responseXML.getElementsByTagName('Message')[0].childNodes[0].textContent);
+        $('#active').removeClass('busy');
+      });
+  }
+
+  function load() {
+    try {
+      var creds = s3_auth.get();
+      S3Ajax.KEY_ID = creds.key;
+      S3Ajax.SECRET_KEY = creds.secret;
+      list();
+    }
+    catch(e) {
+        console.log("error", e)
+    };
+  }
+
+  this.save = function() {
+    function trim(val) {
+      if (val) {
+        return val.replace(/^\s+|\s+$/g, '');
+      }
+    }
+
+    var key = trim($('#s3-key').val());
+    var secret = trim($('#s3-secret-key').val());
+
+    if (key && key.length > 0) {
+      s3_auth.set(key, secret);
+    }
+    else {
+      s3_auth.clear();
+    }
+
+    window.location = window.location.href;
+  };
+
+  var creds = s3_auth.get();
+
+  if (creds) {
+    $('#s3-key').val(creds.key);
+    $('#s3-secret-key').val(creds.secret);
+  }
+
+  load();
+}
+
+$(function() {
+  s3_controller = new s3Control();
+});
---
+++ keys.xsl
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!--
+     Copyright Jesse Andrews, 2005-2008
+     http://overstimulate.com
+
+     This file may be used under the terms of of the
+     GNU General Public License Version 2 or later (the "GPL"),
+     http://www.gnu.org/licenses/gpl.html
+
+     Software distributed under the License is distributed on an "AS IS" basis,
+     WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+     for the specific language governing rights and limitations under the
+     License.
+-->
+
+<xsl:stylesheet version="1.0"
+  xmlns:S3="http://s3.amazonaws.com/doc/2006-03-01/"
+  xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+<xsl:template match="/">
+  <table cellspacing="0" id="keylist">
+    <thead>
+      <tr>
+        <th align="left">Key</th>
+        <th align="left">Last Modified</th>
+        <th align="left">Size</th>
+        <!-- <th align="left" class="delete_button" width="80">Action</th> -->
+      </tr>
+    </thead>
+    <tbody>
+      <xsl:for-each select="S3:ListBucketResult/S3:CommonPrefixes">
+        <tr class="s3dir">
+          <td><a href="#" onclick="browse(this.getAttribute('prefix')); return false"><xsl:attribute name="prefix"><xsl:value-of select="S3:Prefix"/></xsl:attribute><xsl:value-of select="S3:Key"/><xsl:value-of select="S3:Prefix"/></a></td>
+          <td colspan="3"></td>
+        </tr>
+      </xsl:for-each>
+      <xsl:for-each select="S3:ListBucketResult/S3:Contents">
+        <tr>
+          <xsl:attribute name="key"><xsl:value-of select="S3:Key"/></xsl:attribute>
+          <td><a href="#" onclick="view(this.getAttribute('key')); return false"><xsl:attribute name="key"><xsl:value-of select="S3:Key"/></xsl:attribute><xsl:value-of select="S3:Key"/></a></td>
+          <td><xsl:value-of select="S3:LastModified"/></td>
+          <td><xsl:value-of select="S3:Size"/></td>
+<!--          <td class="actions"><a href="#" class="delete" onclick="deleteKey(this.parentNode.parentNode); return false">delete</a></td> -->
+        </tr>
+      </xsl:for-each>
+    </tbody>
+  </table>
+</xsl:template>
+
+</xsl:stylesheet>
+
